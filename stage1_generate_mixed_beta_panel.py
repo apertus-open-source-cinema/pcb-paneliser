cead20cf8c74fe42ce4c237885af67fddf30f965
Sebastian Pichelhofer <sp-apertus@users.noreply.github.com>
2020-11-18 22:28:26 +0100

    added test strip to stage 1

--- stage1_generate_mixed_beta_panel.py
+++ stage1_generate_mixed_beta_panel.py
@@ -217,6 +217,9 @@ def main():
     add_pcb("axiom_beta_interface_dummy_v0.13_r1.3", 57.15 + cutout_width, 0)
     add_pcb("axiom_beta_main_board_v0.37_r1.1", 0, 57.15 + cutout_width)
     add_pcb("axiom_beta_power_board_v0.37_r1.2", 57.15 + cutout_width, 57.15 + cutout_width)
+    
+    # impedance test strip
+    add_pcb("test_strip", 90, 175)
 
     area = [0, 0, panel_width, panel_height]
     generate_pcb_bridges(board_cutout_msp, area, cutout_width, 4, 6)

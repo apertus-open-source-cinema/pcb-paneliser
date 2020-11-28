f890496d94e9f93f845c705cb24ddc6c16343ca2
Andrej <b.andrej@cumir.de>
2020-11-28 14:57:36 +0100

    - Added BOM processor, which fills up infos in the BOM

---
+++ bom_processor.py
@@ -0,0 +1,105 @@
+import csv
+import os
+
+BOM_FILE = "input/BOM/BOM.tsv"
+EAGLE_DATA_DIR = "input/EAGLE/"
+OUTPUT_DIR = "output_bom/"
+csv_data = []
+
+top_components = {}
+bottom_components = {}
+
+field_names = []
+
+
+def determine_side(components, pcb_name):
+    top = set(components) <= set(top_components[pcb_name])
+    bottom = set(components) <= set(bottom_components[pcb_name])
+
+    side = "N/A"
+    if top and bottom:
+        side = "UNCLEAR"
+    if top:
+        side = "TOP"
+    if bottom:
+        side = "BOT"
+    return side
+
+
+def rename_pcb_components(components, pcb_name):
+    components_pcb = []
+    for component in components:
+        components_pcb.append(format(component + "_" + pcb_name))
+    return components_pcb
+
+
+def load_bom_components():
+    with open(BOM_FILE) as bom_file:
+        for i in range(4):
+            next(bom_file)
+        reader = csv.DictReader(bom_file, delimiter='\t')
+        global field_names
+        field_names = reader.fieldnames
+        for row in reader:
+            if row["ID"] == '':
+                continue
+
+            pcb_name = row["PCB"][:2]
+
+            components = row["Schematic Reference (Silkscreen)"].split(',')
+            components = [x.strip(' ') for x in components]
+
+            components_pcb = rename_pcb_components(components, pcb_name)
+            row["Combined Sch. Reference (*.asc)"] = ",".join(components_pcb)
+
+            side = determine_side(components, pcb_name)
+            row["Side"] = side
+            csv_data.append(row)
+
+
+def get_components_list(components, file):
+    for line in file:
+        name, x, y, rotation, *temp = line.split()
+        components.append(name)
+
+
+def get_components(pcb_name, suffix):
+    file = open(EAGLE_DATA_DIR + pcb_name + "/" + pcb_name + ".mnt", "r")
+    components = []
+    get_components_list(components, file)
+    top_components[suffix] = components
+
+    file = open(EAGLE_DATA_DIR + pcb_name + "/" + pcb_name + ".mnb", "r")
+    components = []
+    get_components_list(components, file)
+    bottom_components[suffix] = components
+
+
+def write_bom():
+    with open(OUTPUT_DIR + 'test_bom.csv', 'w', newline='') as f:
+        writer = csv.DictWriter(f, field_names, delimiter=";")
+        writer.writeheader()
+        writer.writerows(csv_data)
+
+
+def setup():
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+
+
+def main():
+    setup()
+
+    get_components("axiom_beta_main_board_v0.37_r1.1", "MB")
+    get_components("axiom_beta_power_board_v0.37_r1.2", "PB")
+    get_components("axiom_beta_interface_dummy_v0.13_r1.3", "IB")
+    get_components("axiom_beta_sensor_cmv12000_tht_v0.16_r1.5c", "SB")
+
+    load_bom_components()
+
+    write_bom()
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()

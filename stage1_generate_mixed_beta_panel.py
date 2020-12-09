d84db359182fb45053d4b385d681acbd8f221d90
Sebastian Pichelhofer <sp-apertus@users.noreply.github.com>
2020-12-09 16:08:13 +0100

    rotated gerbers and updated versions in panelizer script

--- stage1_generate_mixed_beta_panel.py
+++ stage1_generate_mixed_beta_panel.py
@@ -224,10 +224,10 @@ def main():
     # Commented out to disable outer frame for mixed panel (stage 2)
     generate_outer_frame(board_cutout_msp, panel_width, panel_height)
 
-    add_pcb("axiom_beta_sensor_cmv12000_tht_v0.16_r1.6c", 0, 0)
-    add_pcb("axiom_beta_interface_dummy_v0.13_r1.4", 57.15 + cutout_width, 0)
-    add_pcb("axiom_beta_main_board_v0.38", 0, 57.15 + cutout_width)
-    add_pcb("axiom_beta_power_board_v0.38", 57.15 + cutout_width, 57.15 + cutout_width)
+    add_pcb("axiom_beta_sensor_cmv12000_tht_v0.16_r1.7c", 0, 0)
+    add_pcb("axiom_beta_interface_dummy_v0.13_r1.5", 57.15 + cutout_width, 0)
+    add_pcb("axiom_beta_main_board_v0.38_r1.1", 0, 57.15 + cutout_width)
+    add_pcb("axiom_beta_power_board_v0.38_r1.1", 57.15 + cutout_width, 57.15 + cutout_width)
 
     # impedance test strip
     add_pcb("test_strip_v0.1_r1.2", 0.1, 0.1, generate_frame=False, merge_outline=False)

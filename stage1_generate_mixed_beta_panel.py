f9a9c640db8ae1fd654f755c81a3631351bf5826
Sebastian Pichelhofer <sp-apertus@users.noreply.github.com>
2020-12-18 20:53:08 +0100

    added new version gerber output and stage1 script adaptions

--- stage1_generate_mixed_beta_panel.py
+++ stage1_generate_mixed_beta_panel.py
@@ -171,7 +171,7 @@ def place_panel_label(x, y):
 
 def place_subpanel_label(x, y):
     font.normalize_rendering(2)
-    label_text = "AXIOM Beta Mixed Subpanel - Version 0.42 - Date: " + today.strftime("%d.%m.%Y") + " (DD.MM.YYYY)"
+    label_text = "AXIOM Beta Mixed Subpanel - Version 0.43 - Date: " + today.strftime("%d.%m.%Y") + " (DD.MM.YYYY)"
     draw_text(label_text, x, y, True)
 
 
@@ -242,10 +242,10 @@ def main():
     # Commented out to disable outer frame for mixed panel (stage 2)
     generate_outer_frame(board_cutout_msp, panel_width, panel_height)
 
-    add_pcb("axiom_beta_sensor_cmv12000_tht_v0.16_r1.7c", 0, 0)
+    add_pcb("axiom_beta_sensor_cmv12000_tht_v0.16_r1.8c", 0, 0)
     add_pcb("axiom_beta_interface_dummy_v0.13_r1.6", 57.15 + cutout_width, 0)
-    add_pcb("axiom_beta_main_board_v0.38_r1.1", 0, 57.15 + cutout_width)
-    add_pcb("axiom_beta_power_board_v0.38_r1.1", 57.15 + cutout_width, 57.15 + cutout_width)
+    add_pcb("axiom_beta_main_board_v0.38_r1.2", 0, 57.15 + cutout_width)
+    add_pcb("axiom_beta_power_board_v0.38_r1.2", 57.15 + cutout_width, 57.15 + cutout_width)
 
     # impedance test strip
     add_pcb("test_strip_v0.1_r1.2", 0.1, 0.1, generate_frame=False, merge_outline=False)

688fb10f7cee6b94494e9a18a66518281a593a65
Sebastian Pichelhofer <sp-apertus@users.noreply.github.com>
2021-01-07 19:42:57 +0100

    added rotation scripts

---
+++ input/tele_variant/rotate_90_ccw.sh
@@ -0,0 +1,13 @@
+mkdir output_rotated
+for f in $1/*.ger;  
+do echo ${f};
+gerbv ${f} -x rs274x -umm -T0x0r90 -o output_rotated/${f##*/}
+done;
+
+for f in $1/*.xln; 
+do echo ${f};
+gerbv ${f} -x drill -umm -T0x0r90 -o output_rotated/${f##*/}
+done;
+
+mv $1 $1_original
+mv output_rotated $1
